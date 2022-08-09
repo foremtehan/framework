867274bd78b73f0069baa02d04ea45147cea5fd0
foremtehan <53290883+foremtehan@users.noreply.github.com>
2022-08-09 12:57:47 +0430

    Update helpers.php

--- src/Illuminate/Collections/helpers.php
+++ src/Illuminate/Collections/helpers.php
@@ -179,11 +179,15 @@ if (! function_exists('value')) {
     /**
      * Return the default value of the given value.
      *
-     * @param  mixed  $value
+     * @param mixed $value
      * @return mixed
      */
     function value($value, ...$args)
     {
+        if ($value instanceof \BackedEnum) {
+            return $value->value;
+        }
+
         return $value instanceof Closure ? $value(...$args) : $value;
     }
 }
